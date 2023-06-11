822b650514d4b8c5512c416b53fe83f691207621
Alex <ndsvw@gmx.de>
2023-06-11 15:25:14 +0200

    Use metas that are not part of a head tag and added support for "itemprop" property of metas

--- content.js
+++ content.js
@@ -4,14 +4,17 @@ browser.runtime.onMessage.addListener(request => {
             return Promise.resolve({ response: true });
         }
         else if (request.query == "get-metas") {
-            let metaTags = Array.from(document.head.querySelectorAll("meta"));
+            let metaTags = Array.from(document.querySelectorAll("meta"));
             let metasWithNameAttribute = metaTags
                 .filter(x => x?.attributes?.name && x?.attributes?.content)
                 .map(x => ({ name: x.attributes.name.value, content: x.attributes.content.value }));
             let metasWithPropertyAttribute = metaTags
                 .filter(x => x?.attributes?.property && x?.attributes?.content)
                 .map(x => ({ name: x.attributes.property.value, content: x.attributes.content.value }));
-            let metasArray = metasWithNameAttribute.concat(metasWithPropertyAttribute);
+            let metasWithItempropAttribute = metaTags
+                .filter(x => x?.attributes?.itemprop && x?.attributes?.content)
+                .map(x => ({ name: x.attributes.itemprop.value, content: x.attributes.content.value }));
+            let metasArray = metasWithNameAttribute.concat(metasWithPropertyAttribute).concat(metasWithItempropAttribute);
             return Promise.resolve({ response: JSON.stringify(metasArray) });
         }
         else if (request.query == "get-ld-jsons") {
